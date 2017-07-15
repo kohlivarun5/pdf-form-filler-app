d8e5933b703ed23e581a33b2645a93d176f8335a
Varun Kohli <kohli.varun5@gmail.com>
2017-07-15 17:52:17 -0400

    .

--- src/pages/forms/i539.ts
+++ src/pages/forms/i539.ts
@@ -3,19 +3,32 @@ import { Component } from '@angular/core';
 templateUrl: 'i539.html'
 })
 export class I539 {
-fields = [
-    {
-      label:"Family Name",
-      value:"",
-      pdf_field:""
-    },
-    {
-      label:"Given Name",
-      value:"",
-      pdf_field:""
-    },
+
+data = {
+  currentGroup : 0,
+  groups : [
+    [
+      {
+        "label":"Family Name",
+        "title": "form1[0].#subform[0].P1_Line3a_FamilyName[0]",
+        "fieldType": "Text",
+        "fieldFlags": "8388608",
+        "fieldValue": ""
+      },
+      {
+        "label":"Given Name",
+        "title": "form1[0].#subform[0].P1_Line3b_GivenName[0]",
+        "fieldType": "Text",
+        "fieldFlags": "8388608",
+        "fieldValue": ""
+        }
+    ]
   ]
+}
+
+currentGroup = 0;
+
   logForm() {
-    console.log(this.fields)
+    console.log(this.data)
   }
 }
